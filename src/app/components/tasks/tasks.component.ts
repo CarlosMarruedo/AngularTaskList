93fc15edfba77216feab0286ea66444eeb3c01c4
carlosmarruedo <you@example.com>
2021-05-22 17:55:25 +0200

    created fake db and get task from it

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
+
+
 
 @Component({
   selector: 'app-tasks',
@@ -9,11 +11,14 @@ import { TASKS } from '../../mock-tasks';
 })
 export class TasksComponent implements OnInit {
 
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    //done with observables
+    //because reciving from database, async
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
   }
 
 }
