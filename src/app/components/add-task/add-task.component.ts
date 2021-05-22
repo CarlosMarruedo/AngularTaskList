161c4311088bb41b08d07c3e09a8984106c21b1e
carlosmarruedo <you@example.com>
2021-05-22 20:17:36 +0200

    add ui improvements, added routes and limited routes

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {Task} from "../../Task"
+import {UiService} from '../../services/ui.service';
+import {Subscription} from 'rxjs';
+
 
 @Component({
   selector: 'app-add-task',
@@ -7,12 +10,17 @@ import {Task} from "../../Task"
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
   text!: string;
   day!: string;
   reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription!: Subscription;
 
-  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
-  constructor() { }
+  
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value)  => this.showAddTask = value)
+   }
 
   ngOnInit(): void {
   }
